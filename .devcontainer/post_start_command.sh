d3d2fa1f7ad346a72ee464b198461ad2d7f752fb
Florian Monteghetti <fm_bino@hotmail.com>
2023-07-19 16:40:50 +0200

    Bump dolfinx version to v0.5.0 (all demos have been checked)

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -3,3 +3,7 @@
 containerWorkspace="$1"
 # Local editable install of scientific-computing-utils package
 pip install -e ${containerWorkspace}
+# Launch X virtual framebuffer in the background (neede for pyvista)
+# (Check with ps -ef | grep Xvfb; the 'sleep 3' is important.)
+nohup Xvfb :99 -screen 0 800x600x24 &
+sleep 3
