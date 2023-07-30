ee9809692c0d2c3e86ad3ddfb746de5163ba7a57
Florian Monteghetti <fm_bino@hotmail.com>
2023-07-30 11:25:43 +0200

    Switch to complex PetscScalar by default, and fix dolfinx_mpc compilation (all demos tested).

--- .devcontainer/build_complex_dolfinx_mpc.sh
+++ .devcontainer/build_complex_dolfinx_mpc.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # Compile dolfinx_mpc with complex PetscScalar.
 # This script must be run with 'source'.
-echo "Switching dolfinx to complex mode..."
+cd /tmp/dolfinx_mpc
 . /usr/local/bin/dolfinx-complex-mode
+. /usr/local/dolfinx-complex/lib/dolfinx/dolfinx.conf
 echo "Compiling dolfinx_mpc..."
-cd /tmp/dolfinx_mpc/cpp
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build-dir ../cpp/
-ninja -j3 install -C build-dir
-cd .. && pip3 install python/. --upgrade
+rm -rf build-dir
+rm -rf python/build 
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build-dir cpp/
+ninja -j`nproc` install -C build-dir
+pip3 install python/. --upgrade
 echo "Compiling dolfinx_mpc...DONE"
