72a0d7c84032284153def0c47118b273b306bd73
Florian Monteghetti <fmonteghetti@users.noreply.github.com>
2024-05-15 09:47:52 +0200

    - Improve test for kron_vector: real and complex vectors

--- demos/PETSc-SLEPc-SciPy/test_kron_vector.py
+++ demos/PETSc-SLEPc-SciPy/test_kron_vector.py
@@ -18,8 +18,8 @@ from petsc4py import PETSc
 import numpy as np
 from scientific_computing_utils import PETSc_utils
 
-def _create_vector(N):
-    """ Create distributed vector. """
+def _get_integer_test(N):
+    """ Create a distributed integer vector L and the matrix L*L^T."""
     L = PETSc.Vec()
     L.create(comm)
     L.setSizes((PETSc.DECIDE,N))
@@ -29,39 +29,74 @@ def _create_vector(N):
     L.setValues(rows_idx,rows_idx)
     L.assemblyBegin()
     L.assemblyEnd()
-    return L
 
-def _compute_exact_solution(N):
-    """ Exact kronecker product L*L^T. """
     A_ex = np.kron(np.arange(0,N),np.arange(0,N)).reshape((N,-1))
-    return A_ex
+    return (L,A_ex)
+
+def _get_real_test(N):
+    """ Create a distributed real vector L and the matrix L*L^T."""
+    x = np.linspace(-10,10,num=N)
+    f = lambda x: np.exp(x) * np.sin(2*x) + 1.23 
+    x = f(x)
+    A_ex = np.kron(x,x).reshape(N,-1)
+    L = PETSc.Vec()
+    L.create(comm)
+    L.setSizes((PETSc.DECIDE,N))
+    L.setFromOptions()
+    rows=L.getOwnershipRange()
+    rows_idx = np.arange(rows[0],rows[1],dtype=PETSc.IntType)
+    L.setValues(rows_idx,x[rows_idx])
+    L.assemblyBegin()
+    L.assemblyEnd()
+    return (L,A_ex)
+
+def _get_complex_test(N):
+    """ Create a distributed complex vector L and the matrix L*L^T."""
+    x = np.linspace(-10,10,num=N)
+    f = lambda x: np.exp(x) * np.sin(2*x) + 1.23 
+    x = f(x + 1j)
+    A_ex = np.kron(x,x).reshape(N,-1)
+
+    L = PETSc.Vec()
+    L.create(comm)
+    L.setSizes((PETSc.DECIDE,N))
+    L.setFromOptions()
+    rows=L.getOwnershipRange()
+    rows_idx = np.arange(rows[0],rows[1],dtype=PETSc.IntType)
+    L.setValues(rows_idx,x[rows_idx])
+    L.assemblyBegin()
+    L.assemblyEnd()
+    return (L,A_ex)
 
 def _compute_error(A,A_ex):
     """ Compute ||A-Aex|| in the infinity norm."""
     I = A.getOwnershipRange()
     error = 0
     for i in np.arange(I[0],I[1]):
-        for j in np.arange(0,N):
+        for j in np.arange(0,A_ex.shape[1]):
             error = np.max([error,np.abs(A[i,j]-A_ex[i,j])])
     return error
 
-N = 1234 
-L=_create_vector(N)
-t0=time.process_time_ns()
-A = PETSc_utils.kron_vector(L)
-comm.Barrier()
-if comm.rank ==0:
-    print(f"Elapsed time: {1e-9*(time.process_time_ns()-t0):1.2g}s")
-A_ex = _compute_exact_solution(N)
-error = _compute_error(A,A_ex)
-print(f"[{comm.rank}] Error={error}")
+N = 239
+test_cases = [_get_integer_test(N+27),
+              _get_real_test(N+28)]
+if PETSc.ScalarType==np.complex128:
+    test_cases.append(_get_complex_test(N+33))
 
-# Test reuse of matrix A
-A.zeroEntries()
-t0=time.process_time_ns()
-PETSc_utils.kron_vector(L,result=A)
-comm.Barrier()
-if comm.rank ==0:
-    print(f"Elapsed time (re-use): {1e-9*(time.process_time_ns()-t0):1.2g}s")
-error = _compute_error(A,A_ex)
-print(f"[{comm.rank}] Error={error}")
+for (L,A_ex) in test_cases:
+    t0=time.process_time_ns()
+    A = PETSc_utils.kron_vector(L)
+    comm.Barrier()
+    if comm.rank ==0:
+        print(f"Elapsed time: {1e-9*(time.process_time_ns()-t0):1.2g}s")
+    error = _compute_error(A,A_ex)
+    print(f"[{comm.rank}] Error={error}")
+    # Test reuse of matrix A
+    A.zeroEntries()
+    t0=time.process_time_ns()
+    PETSc_utils.kron_vector(L,result=A)
+    comm.Barrier()
+    if comm.rank ==0:
+        print(f"Elapsed time (re-use): {1e-9*(time.process_time_ns()-t0):1.2g}s")
+    error = _compute_error(A,A_ex)
+    print(f"[{comm.rank}] Error={error}")
