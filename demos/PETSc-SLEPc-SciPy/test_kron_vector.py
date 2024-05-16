b16bdd5dba0469123608d01fc062198566212ef3
Florian Monteghetti <fmonteghetti@users.noreply.github.com>
2024-05-16 15:54:59 +0200

    Reduce error to all processes

--- demos/PETSc-SLEPc-SciPy/test_kron_vector.py
+++ demos/PETSc-SLEPc-SciPy/test_kron_vector.py
@@ -75,7 +75,7 @@ def _compute_error(A,A_ex):
     for i in np.arange(I[0],I[1]):
         for j in np.arange(0,A_ex.shape[1]):
             error = np.max([error,np.abs(A[i,j]-A_ex[i,j])])
-    return error
+    return comm.allreduce(error,op=MPI.SUM)
 
 N = 239
 test_cases = [_get_integer_test(N+27),
@@ -90,7 +90,8 @@ for (L,A_ex) in test_cases:
     if comm.rank ==0:
         print(f"Elapsed time: {1e-9*(time.process_time_ns()-t0):1.2g}s")
     error = _compute_error(A,A_ex)
-    print(f"[{comm.rank}] Error={error}")
+    if comm.rank==0:
+        print(f"error: {error}")
     # Test reuse of matrix A
     A.zeroEntries()
     t0=time.process_time_ns()
@@ -99,8 +100,9 @@ for (L,A_ex) in test_cases:
     if comm.rank ==0:
         print(f"Elapsed time (re-use): {1e-9*(time.process_time_ns()-t0):1.2g}s")
     error = _compute_error(A,A_ex)
-    print(f"[{comm.rank}] Error={error}")
+    if comm.rank==0:
+        print(f"Error: {error}")
 # Print memory allocation info
 info = A.getInfo()
 if comm.rank==0:
-    print(f"[{comm.rank}] nz_allocated/nz_unneeded/mallocs: {info['nz_allocated']}/{info['nz_unneeded']}/{info['mallocs']}.")
+    print(f"nz_allocated/nz_unneeded/mallocs: {info['nz_allocated']}/{info['nz_unneeded']}/{info['mallocs']}.")
