2ff375da7e49c345ab9ae6d38d8f2a7aa35cb576
Florian Monteghetti <fmonteghetti@users.noreply.github.com>
2024-08-29 13:11:51 +0200

    .devcontainer.json providing a working environment for dolfinx 0.6 with dolfinx_mpc

--- scicomp_utils_dolfinx/.devcontainer/post_create_command.sh
+++ scicomp_utils_dolfinx/.devcontainer/post_create_command.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # Setup docker environment.
+containerWorkspace="$1"
 # Upgrade pip (old versions to do not support local editable installs)
 pip3 install --upgrade pip
+# Needed for gmsh
+ln -s /usr/bin/python3 ~/.local/bin/python 
+# Local editable install
+pip install -e ${containerWorkspace}
+# For vscode jupyter extension
+pip install ipykernel pandas
--- scicomp_utils_dolfinx/.devcontainer/post_start_command.sh
+++ scicomp_utils_dolfinx/.devcontainer/post_start_command.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # Setup docker environment.
 containerWorkspace="$1"
-# Local editable install
-pip install -e ${containerWorkspace}
 # Launch X virtual framebuffer in the background (neede for pyvista)
 # (Check with ps -ef | grep Xvfb; the 'sleep 3' is important.)
 nohup Xvfb :99 -screen 0 800x600x24 &
